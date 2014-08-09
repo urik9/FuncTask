34626a35654e50e6ae2b089b8609e7fdff1e3ba5
urik9 <uri@wix.com>
2014-08-10 00:01:00 +0300

    stuck on adding to list

--- src/main/scala/FuncDataTransform.scala
+++ src/main/scala/FuncDataTransform.scala
@@ -1,25 +1,37 @@
-import java.util
+
 
 /**
  * Created by Uri_Keinan on 8/5/14.
  */
 class FuncDataTransform {
-  def count (lri: List[LoggedItem]) = lri.size
+  def count(lri: List[LoggedItem]) = lri.size
 
   def responseSize(lrp: List[LoggedResponse]) = {
-    var rs =0
-    for(rp <- lrp)
-      rs = rs+ rp.respSize
+    var rs = 0
+    for (rp <- lrp)
+      rs = rs + rp.respSize
     rs
   }
 
-  def errorRate(lrq: List[LoggedRequest]): Double  = {
+  def errorRate(lrq: List[LoggedRequest]): Double = {
     var err: Double = 0.0
-    for(rq <- lrq) {
+    for (rq <- lrq) {
       if (rq.status != "200")
         err = err + 1
     }
-        err/ count(lrq)
-      }
+    err / count(lrq)
+  }
+
+  def errorRateSpecific(lrq: List[LoggedRequest]) = {
+    val errorRateByType: List[Double] = List.empty
+    val resultlist = lrq.groupBy(rq => rq.status.charAt(0))
+    for (result <- resultlist) {
+      errorRateByType :+ errorRate(result._2)
+    }
+    errorRateByType
+
+
+  }
+
 
 }
