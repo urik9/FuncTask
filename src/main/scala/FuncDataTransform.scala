b934941c5b0813edb1e074a3b4170588e8976d89
urik9 <uri@wix.com>
2014-08-10 22:07:34 +0300

    matcher issue with test 5

--- src/main/scala/FuncDataTransform.scala
+++ src/main/scala/FuncDataTransform.scala
@@ -1,4 +1,4 @@
-
+import scala.collection.immutable.Nil
 
 /**
  * Created by Uri_Keinan on 8/5/14.
@@ -6,32 +6,33 @@
 class FuncDataTransform {
   def count(lri: List[LoggedItem]) = lri.size
 
-  def responseSize(lrp: List[LoggedResponse]) = {
-    var rs = 0
-    for (rp <- lrp)
-      rs = rs + rp.respSize
-    rs
+  def responseSize(loggedResponses: List[LoggedResponse]) = {
+    loggedResponses.foldLeft(0)((acc, response) => acc + response.respSize)
   }
 
-  def errorRate(lrq: List[LoggedRequest]): Double = {
-    var err: Double = 0.0
-    for (rq <- lrq) {
-      if (rq.status != "200")
-        err = err + 1
-    }
-    err / count(lrq)
-  }
-
-  def errorRateSpecific(lrq: List[LoggedRequest]) = {
-    val errorRateByType: List[Double] = List.empty
-    val resultlist = lrq.groupBy(rq => rq.status.charAt(0))
-    for (result <- resultlist) {
-      errorRateByType :+ errorRate(result._2)
-    }
-    errorRateByType
+  def errorRate(loggedRequests: List[LoggedRequest]): Double = {
+    loggedRequests.foldLeft(0)((acc, request) => (request.status) match {
+      case ("200") => acc
+      case (_) => acc + 1
+    })
+      .toDouble / count(loggedRequests)
 
 
   }
 
+  def errorRateSpecific(loggedRequests: List[LoggedRequest]) = {
+    loggedRequests.filterNot(request => request.status == "200")
+      .groupBy(request => request.status.charAt(0)).mapValues(value => count(value).toDouble / count(loggedRequests))
+      .toList.sortBy(res => res._1)
+
+  }
+
+  //    val errorRateByType: List[Double] = List.empty
+  //    val resultlist = lrq.groupBy(rq => rq.status.charAt(0))
+  //    for (result <- resultlist) {
+  //     println(count(result._2))
+  //     errorRateByType :+ (count(result._2))
+  //    }
+  //    errorRateByType
 
 }
