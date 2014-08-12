b5ab23cf77d048c61f4559ac481763dfbbef65cb
urik9 <uri@wix.com>
2014-08-12 23:27:34 +0300

    Trying to return several values per function

--- src/main/scala/FuncDataTransform.scala
+++ src/main/scala/FuncDataTransform.scala
@@ -1,9 +1,10 @@
-import scala.collection.immutable.Nil
+
 
 /**
  * Created by Uri_Keinan on 8/5/14.
  */
 class FuncDataTransform {
+
   def count(lri: List[LoggedItem]) = lri.size
 
   def responseSize(loggedResponses: List[LoggedResponse]) = {
@@ -24,15 +25,26 @@ class FuncDataTransform {
     loggedRequests.filterNot(request => request.status == "200")
       .groupBy(request => request.status.charAt(0)).mapValues(value => count(value).toDouble / count(loggedRequests))
       .toList.sortBy(res => res._1)
-
   }
 
-  //    val errorRateByType: List[Double] = List.empty
-  //    val resultlist = lrq.groupBy(rq => rq.status.charAt(0))
-  //    for (result <- resultlist) {
-  //     println(count(result._2))
-  //     errorRateByType :+ (count(result._2))
-  //    }
-  //    errorRateByType
+  def getByMostPopularUrls(requests: List[LoggedRequest]) = {
+    requests.groupBy(request => request.url).toArray.sortBy(request => count(request._2)).reverse
+
+  }
+  def getInfoForURL (requests: List[LoggedRequest]) = {
+    val urlToInfo = getByMostPopularUrls(requests)
+    errorRate(urlToInfo(0)._2)/count(urlToInfo(0)._2)
+      urlToInfo(0)._1
+
+
+
+
+    //    urlToInfo(0)._2 match {
+    //      case ("GET",_, "200") => List(LoggedResponse(_, 200))
+    //      case ("GET",_,_)=> new LoggedError(_)
+    //      case ("POST",_,"200")=> List(LoggedResponse(_, 100))
+    //    }
+
+  }
 
 }
